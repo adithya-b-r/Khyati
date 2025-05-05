4fc89c24cf8baf1619e0a4c39f90b81dfe496054
Abr009 <bradithya945@gmail.com>
2025-05-05 10:35:04 +0530

    Added Responsive Navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 
 export const Navbar = () => {
   const navRef = useRef(null);
+  const [menuToggle, setMenuToggle] = useState(false);
 
   useEffect(() => {
     const spans = navRef.current.querySelectorAll("#logo h4 span");
@@ -48,8 +49,34 @@ export const Navbar = () => {
     );
   }, []);
 
+  const toggleMenu = () => {
+    setMenuToggle(!menuToggle);
+
+    const mob = navRef.current.querySelectorAll("#mob i");
+    const mobMenu = navRef.current.querySelector("#mobMenu");
+    const mobItems = navRef.current.querySelectorAll("#mobMenu a");
+
+    gsap.fromTo(
+      mob,
+      {rotate: 0, opacity: 0},
+      {rotate: 180, opacity: 1, duration: 0.8}
+    );
+
+    gsap.fromTo(
+      mobMenu,
+      { height: 0 },
+      { height: "40vh", duration: 1, ease: "power2.out" }
+    );
+    
+    gsap.fromTo(
+      mobItems,
+      { opacity: 0, y: -20},
+      { opacity: 1, y: 0, duration: .8, stagger: 0.15 }
+    );
+  }
+
   return (
-    <nav ref={navRef} className="w-full h-[10vh] bg-blue-600 flex justify-between px-8">
+    <nav ref={navRef} className="w-full h-[10vh] bg-blue-600 flex justify-between pl-5 pr-8">
       <div id="logo" className="flex h-full gap-3 text-white justify-between items-center">
         <img className="w-14 h-14" src="/imgs/logo.png" alt="" />
         <h4 className="font-bold text-2xl">
@@ -62,7 +89,16 @@ export const Navbar = () => {
         </h4>
       </div>
 
-      <div id="menu" className='hidden md:flex h-full justify-between gap-8 items-center text-white font-semibold tracking-wide'>
+      <div id="menu" className={`hidden md:flex h-full justify-between gap-8 items-center text-white font-semibold tracking-wide`}>
+        <a href="">Home</a>
+        <a href="">About Us</a>
+        <a href="">Services</a>
+        <a href="">Gallery</a>
+        <a href="">FAQs</a>
+      </div>
+
+      {/* style={{background: "", backdropFilter: "blur(10px)"}} */}
+      <div id="mobMenu" className={`${!menuToggle ? 'hidden' : 'block'} absolute top-[9.90vh] left-0 h-[40vh] w-[85vw] mx-[7.5vw] flex flex-col justify-around items-center text-white text-xl font-semibold bg-blue-600`} >
         <a href="">Home</a>
         <a href="">About Us</a>
         <a href="">Services</a>
@@ -71,7 +107,8 @@ export const Navbar = () => {
       </div>
 
       <div id='mob' className='md:hidden flex text-white text-3xl items-center font-semibold'>
-      <i class="ri-menu-3-line"></i>
+        <i onClick={toggleMenu}  class={`${menuToggle ? 'hidden' : 'block'} ri-menu-3-line`}></i>
+        <i onClick={toggleMenu}  class={`${!menuToggle ? 'hidden' : 'block'} ri-close-large-line`}></i>
       </div>
     </nav>
   )
